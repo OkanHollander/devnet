14c7a87fa589752691237bc2e2baed75457031fa
o.hollander <o.hollander1990@gmail.com>
2022-11-29 02:04:13 -0500

    Handling the data

--- Nexus/nxapi_cdp.py
+++ Nexus/nxapi_cdp.py
@@ -24,6 +24,7 @@ response = requests.post(url=url,
                          verify=False).json()
 
 # print(response)
+
 ################ LOGIN WITH NX-API REST ################
 auth_url = "http:/192.168.2.12/api/mo/aaaLogin.json"
 auth_body = {
@@ -41,4 +42,26 @@ auth_response = requests.post(url=auth_url,
 token = auth_response["imdata"][0]["aaaLogin"]["attributes"]["token"]
 cookies = {}
 cookies["APIC-cookie"] = token
-print(cookies)
+# print(cookies)
+
+################ HANDLING THE DATA ################
+counter = 0
+neighbor_count = response["ins-api"]["outputs"]["output"]["body"]["neigh_count"]
+# print(neighbor_count)
+while counter < neighbor_count:
+    hostname = response["ins-api"]["outputs"]["output"]["body"]
+    ["TABLE_cdp_neighbor_brief_info"]["ROW_cdp_neighbor_brief_info"][counter]["device_id"]
+    local_int = response["ins-api"]["outputs"]["output"]["body"]
+    ["TABLE_cdp_neighbor_brief_info"]["ROW_cdp_neighbor_brief_info"][counter]["intf_id"]
+    remote_int = response["ins-api"]["outputs"]["output"]["body"]
+    ["TABLE_cdp_neighbor_brief_info"]["ROW_cdp_neighbor_brief_info"][counter]["port_id"]
+    
+    body = {
+        "l1PhysIf": {
+            "attributes": {
+                "descr": "Connected to " + hostname + " Remote interface is " + remote_int
+            }
+        }
+    }
+    
+    counter += 1
